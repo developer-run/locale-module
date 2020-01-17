9db51cc7f586bba9540ba9635ba1e24b30f4cf9e
Pavel Paulík <pavel.paulik@seznam.cz>
2020-01-17 20:44:57 +0100

    first commit

---
+++ Module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * This file is part of the devrun
+ * Copyright (c) 2019
+ *
+ * @file    Module.php
+ * @author  Pavel Paulík <pavel.paulik@support.etnetera.cz>
+ */
+
+namespace Devrun\LanguageModule;
+
+use Devrun\Module\ComposerModule;
+
+class Module extends ComposerModule
+{
+
+}
---
+++ readme.md
@@ -0,0 +1,7 @@
+## Installation
+
+The best way to install devrun/locale-module is using Composer:
+
+```sh
+composer require devrun/locale-module
+```
